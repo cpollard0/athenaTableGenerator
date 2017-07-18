3ccfd3a5c530fdf1b609cecc9dbb499e3a434bf0
cpollard0 <chris.pollard@rocketmail.com>
2017-07-17 21:21:33 -0400

    Add example from AWS docs; get primary working

--- src/main.py
+++ src/main.py
@@ -1,17 +1,38 @@
 import json
-from os import path
+import boto3
 
 tab = '\t'
-sep = ','
+
 serde = ' \n) ROW FORMAT SERDE \'org.openx.data.jsonserde.JsonSerDe\' \
 WITH SERDEPROPERTIES ( \
   \'serialization.format\' = \'1\' \
 ) LOCATION \'s3://private.halocoach.com/data/\' '
 
+valid_type_list = ['INT', 'STRING']
 
-#builds a map of json nodes and how "deep" they go; important for making sure we don't add an extra row dleimitere where we don't need one
-def define_node_count(obj,node_dict,node):
-    for key,value in obj.items():
+
+def exec_athena_query(query):
+    client = boto3.client('athena')
+    response = client.start_query_execution(
+        QueryString=query,
+        # ClientRequestToken='string',
+        # QueryExecutionContext={
+        #    'Database': 'string'
+        # },
+        ResultConfiguration={
+            'OutputLocation': 's3://private.halocoach.com/results',
+            'EncryptionConfiguration': {
+                'EncryptionOption': 'SSE_S3'  # | 'SSE_KMS' | 'CSE_KMS',
+                # 'KmsKey': 'string'
+            }
+        }
+    )
+
+
+# builds a map of json nodes and how "deep" they go; important for making sure
+# we don't add an extra row delimiter where we don't need one
+def define_node_count(obj, node_dict, node):
+    for key, value in obj.items():
         if node in node_dict:
             node_dict[node] = node_dict[node] + 1
         else:
@@ -48,22 +69,27 @@ def recursive_items(dictionary, level=0,node='root'):
                     yield from recursive_items(value[0], level + 1,node + '.' + key)
                     yield (level + 1) * tab + '>>' + sep
                 else:
-                    yield (level * tab + key + spacer + 'array<' + value[0] +'>') 
+                    yield (level * tab + key + spacer + 'array<' + value[0] + '>' + sep)
                 
             except:
                 pass  # print("error in list")
         else:
+            field_data_type = ""
             if isinstance(value, int):
-                yield (level * tab + key + spacer + "int" + sep)
-            else: 
+                field_data_type = "int"
+                # yield (level * tab + key + spacer + "int" + sep)
+            else:
+                field_data_type = "string"
                 # yield (level * '\t' + key + spacer + str(value) + ",")
-                yield (level * tab + key + spacer + str(value) + sep)
+                # yield (level * tab + key + spacer + str(value) + sep)
+            yield (level * tab + key + spacer + field_data_type + sep)
 
 
 data = ""
 
-#fileName = '/Users/Chris/PycharmProjects/HaloCoach/Halo5.json'
-fileName = 'C:\\Users\\cpollard\\Desktop\\athenaGenerator\\athenaTableGenerator\\sampleJsonDocs\\halo5_matchResult.json'
+fileName = '/Users/Chris/PycharmProjects/athenaTableGenerator/sampleJsonDocs/halo5_matchResult.json'
+# fileName = '/Users/Chris/PycharmProjects/athenaTableGenerator/sampleJsonDocs/docs_complex_example.json'
+#fileName = 'C:\\Users\\cpollard\\Desktop\\athenaGenerator\\athenaTableGenerator\\sampleJsonDocs\\halo5_matchResult.json'
 #fileName = 'C:\\Users\\cpollard\\Desktop\\athenaGenerator\\athenaTableGenerator\\sampleJsonDocs\\simple.json'
 
 with open(fileName, 'r') as f:
@@ -77,7 +103,7 @@ for line in lines:
 
 data = json.loads(data)
 
-table_name = "test_table"
+table_name = "halo5_matchresult"
 table_schema = "default"
 table = 'CREATE EXTERNAL TABLE IF NOT EXISTS {}.{}(\n'.format(table_schema,table_name)
 schema = 'default'
@@ -90,13 +116,15 @@ node_dict = {}
 define_node_count(data,node_dict,'root')
 
 for value in recursive_items(data):
-    table = table + value.replace('guid', 'string').replace('uint32', 'int') + '\n'
+    table = table + value + '\n'
 
 # print (table_def)
 #table = table.replace(",>", ">")
+
+table = table + serde
 print(table)
-print(serde)
- 
+
+exec_athena_query(table)
 
 # print(table_def[:-3] + ')' + serde)
 # for key,value in data.items():
